ebc329694364d303171792e4faa43ac122e09f81
fudk <royfu@163.com>
2015-07-23 15:12:38 +0800

    修改调试代码

--- src/main/java/com/greatwall/platform/base/executor/InstantiationTracingBeanPostProcessor.java
+++ src/main/java/com/greatwall/platform/base/executor/InstantiationTracingBeanPostProcessor.java
@@ -16,7 +16,7 @@ public class InstantiationTracingBeanPostProcessor implements ApplicationListene
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		// TODO Auto-generated method stub
-		System.out.println("启动");
+//		System.out.println("启动");
 		//event.getApplicationContext().getDisplayName().equals("Root WebApplicationContext")
 		if(event.getApplicationContext().getParent() == null){//root application context 没有parent，他就是老大. 
 			//需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。 
